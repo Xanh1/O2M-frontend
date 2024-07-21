5ee181669e0c9fd6cfa77b0fad54c94c0eb1ec7c
Miguel Angel <167395866+Xanh1@users.noreply.github.com>
2024-07-21 10:55:30 -0500

    add design graph monitoring

--- src/app/graphicMonitoring/page.jsx
+++ src/app/graphicMonitoring/page.jsx
@@ -1,10 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import { Chart, registerables } from 'chart.js'; // Importa Chart y registerables
+import { Chart, registerables } from "chart.js"; // Importa Chart y registerables
 import { promedio_calidad_por_dia } from "../../hooks/service_monitoring"; // Ajusta la ruta según tu estructura de carpetas
 
-Chart.register(...registerables); // Registra todos los componentes necesarios
+import HeaderMenu from "../../components/HeaderMenu";
+import UserSidebar from "../../components/UserSidebar";
+
+Chart.register(...registerables);
 
 export default function Monitoring() {
   const [chartDataAire, setChartDataAire] = useState({});
@@ -18,27 +21,35 @@ export default function Monitoring() {
         const aguaData = datos.datos.agua;
 
         // Procesa los datos del aire
-        const labelsAire = aireData.map(item => `${item.dia}/${item.mes}/${item.año}`);
-        const dataAire = aireData.map(item => item.promedioCalidadDato);
+        const labelsAire = aireData.map(
+          (item) => `${item.dia}/${item.mes}/${item.año}`
+        );
+        const dataAire = aireData.map((item) => item.promedioCalidadDato);
         setChartDataAire({
           labels: labelsAire,
-          datasets: [{
-            label: 'Calidad del Aire',
-            data: dataAire,
-            backgroundColor: 'rgba(75, 192, 192, 0.6)',
-          }]
+          datasets: [
+            {
+              label: "Calidad del Aire",
+              data: dataAire,
+              backgroundColor: "rgba(75, 192, 192, 0.6)",
+            },
+          ],
         });
 
         // Procesa los datos del agua
-        const labelsAgua = aguaData.map(item => `${item.dia}/${item.mes}/${item.año}`);
-        const dataAgua = aguaData.map(item => item.promedioCalidadDato);
+        const labelsAgua = aguaData.map(
+          (item) => `${item.dia}/${item.mes}/${item.año}`
+        );
+        const dataAgua = aguaData.map((item) => item.promedioCalidadDato);
         setChartDataAgua({
           labels: labelsAgua,
-          datasets: [{
-            label: 'Calidad del Agua',
-            data: dataAgua,
-            backgroundColor: 'rgba(153, 102, 255, 0.6)',
-          }]
+          datasets: [
+            {
+              label: "Calidad del Agua",
+              data: dataAgua,
+              backgroundColor: "rgba(153, 102, 255, 0.6)",
+            },
+          ],
         });
       }
     };
@@ -47,11 +58,30 @@ export default function Monitoring() {
   }, []);
 
   return (
-    <div>
-      <h1>Monitoreo de Calidad del Aire</h1>
-      {chartDataAire.labels && <Bar data={chartDataAire} />}
-      <h1>Monitoreo de Calidad del Agua</h1>
-      {chartDataAgua.labels && <Bar data={chartDataAgua} />}
+    <div className="flex flex-col h-screen">
+      <HeaderMenu />
+      <div className="flex flex-1">
+        <UserSidebar />
+        <div className="flex-1 py-6 sm:ml-64">
+          <div className="py-4 rounded-lg mt-14">
+            <h1 className="text-center mb-4">Monitoreo de Calidad del Aire</h1>
+            <div className="flex justify-center">
+              <div className="max-w-lg w-full">
+                {chartDataAire.labels && <Bar data={chartDataAire} />}
+              </div>
+            </div>
+            <h1 className="text-center mt-6 mb-4">
+              Monitoreo de Calidad del Agua
+            </h1>
+            <div className="flex justify-center">
+              <div className="max-w-lg w-full">
+                {chartDataAgua.labels && <Bar data={chartDataAgua} />}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
