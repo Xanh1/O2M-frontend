15f06465c64de2a4f48447b96301366681c49335
Miguel Angel <167395866+Xanh1@users.noreply.github.com>
2024-07-22 02:56:53 -0500

    add validate token

--- src/hooks/service_auth.js
+++ src/hooks/service_auth.js
@@ -1,4 +1,5 @@
 import { POST } from "./Connection";
+import { GET } from "./Connection";
 
 export async function authPerson(data) {
 
@@ -7,6 +8,22 @@ export async function authPerson(data) {
   try {
 
     datos = await POST("login", data);
+
+  } catch (error) {
+    return error.response.data;
+  }
+
+  return datos.data;
+
+}
+
+export async function validarToken(token) {
+
+  let datos = null;
+
+  try {
+
+    datos = await GET("validar", token);
 
   } catch (error) {
     return error.response.data;
