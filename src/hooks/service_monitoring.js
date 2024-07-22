c42d05f70647c16792fadfcc28e970e901f9cc15
Miguel Angel <167395866+Xanh1@users.noreply.github.com>
2024-07-22 03:06:57 -0500

    Update service_monitoring

--- src/hooks/service_monitoring.js
+++ src/hooks/service_monitoring.js
@@ -1,71 +1,81 @@
-import {GET, POST} from "./Connection";
+import { GET, POST } from "./Connection";
 import Cookies from "js-cookie";
 
-
-
 export async function list_monitoring(token) {
-    let datos = null;
+  let datos = null;
 
-    try {
-        datos = await GET('monitoring/list', token);
-        console.log("aqui todo bien en listar monitoring");
-    } catch (error) {
-        console.log("Error en listar_lotes: ", error.response);
-        return { "code": 500 };
-    }
-    return datos.data;
+  try {
+    datos = await GET("monitoring/list", token);
+    console.log("aqui todo bien en listar monitoring");
+  } catch (error) {
+    console.log("Error en listar_lotes: ", error.response);
+    return { code: 500 };
+  }
+  return datos.data;
 }
-
-
 
 export async function promedio_calidad_por_dia() {
-    let datos = null;
+  let datos = null;
 
-    try {
-        datos = await GET('monitoring/promedio/todo');
-        console.log("aqui todo bien en listar monitoring");
-    } catch (error) {
-        console.log("Error en listar_lotes: ", error.response);
-        return { "code": 500 };
-    }
-    return datos.data;
+  try {
+    datos = await GET("monitoring/promedio/todo");
+    console.log("aqui todo bien en listar monitoring");
+  } catch (error) {
+    console.log("Error en listar_lotes: ", error.response);
+    return { code: 500 };
+  }
+  return datos.data;
 }
 export async function save_monitoring(data, token) {
-    let datos = null;
+  let datos = null;
 
-    try {
-        datos = await POST("monitoring/save", data, token);
-        console.log("######Aqui en service?" + datos);
-        
-    } catch (error) {
-        return error.data;
-
-    }
-    return datos.data;
+  try {
+    datos = await POST("monitoring/save", data, token);
+    console.log("######Aqui en service?" + datos);
+  } catch (error) {
+    return error.data;
+  }
+  return datos.data;
 }
 
-export async function modify_monitoring(data, external/*, token*/){
-    let datos = null;
-    try{
-        datos = await POST('monitoring/modify/' + external, data/*, token*/);
-    }
-    catch(error){
-        return error.data;
-    }
-    return datos.data
+export async function modify_monitoring(data, external /*, token*/) {
+  let datos = null;
+  try {
+    datos = await POST("monitoring/modify/" + external, data /*, token*/);
+  } catch (error) {
+    return error.data;
+  }
+  return datos.data;
 }
 
-
-
-export async function search_person(external/*, token*/){
-    let datos = null;
-    try{
-        datos = await GET('person/search/uid/'+external/*, token*/);
-    }
-    catch(error){
-        return error.response.data;
-    }
-    return datos.data
+export async function search_person(external /*, token*/) {
+  let datos = null;
+  try {
+    datos = await GET("person/search/uid/" + external /*, token*/);
+  } catch (error) {
+    return error.response.data;
+  }
+  return datos.data;
 }
 
+export async function extrapolate_data_aire(data) {
+  let response = null;
+  try {
+    response = await POST("/monitoring/extrapolar/aire", data);
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
+  return response.data;
+}
 
+export async function extrapolate_data_agua(data) {
+  let response = null;
+  try {
+    response = await POST("/monitoring/extrapolar/agua", data);
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
+  return response.data;
+}
